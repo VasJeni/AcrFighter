787958e270ce917d8677103b4cd14114a30c8aae
JasJeni <Vasilyev508@gmail.com>
2018-04-06 13:45:31 +0300

    add gitignore

--- ArcFighter/arc_fighter_alfa.pas
+++ ArcFighter/arc_fighter_alfa.pas
@@ -1,17 +1,18 @@
-{
-	*Версия 1. Поле*
-На экране базовое игровое поле, базовый фон. При нажатии определённой 
-клавиши - программа закрывается.
+{*План работы.*
 
-*Версия 2. Игрок*
-Посредине добавляется фигура игрока (пусть для начала это будет прямоугольник). 
-При нажатии определённых клавиш он меняет своё положение.
+Это приблизительный план, скорее направление. Пункты плана необязательно будут одинаковые по усилиям/времени.
+Он не обязателен к исполнению: захотите что-то сделать раньше, а что-то опустить - пожалуйста.
 
-*Версия 3. Враг*
-Где-то на поле добавляется фигура врага (прямоугольник). Игрок может ходить вокруг 
-врага (это должно быть видно), но не может зайти на его место.
+*Версия 1. Поле* //done
+На экране базовое игровое поле, базовый фон. При нажатии определённой клавиши - программа закрывается.
 
-*Версия 4. Удар*
+*Версия 2. Игрок* //done
+Посредине добавляется фигура игрока (пусть для начала это будет прямоугольник). При нажатии определённых клавиш он меняет своё положение.
+
+*Версия 3. Враг* //done
+Где-то на поле добавляется фигура врага (прямоугольник). Игрок может ходить вокруг врага (это должно быть видно), но не может зайти на его место.
+
+*Версия 4. Удар* //done
 По нажатию кнопки игрок наносит удар: меняется его изображение. Если в хитбоксе удара игрока был враг - враг исчезает.
 
 *Версия 5. Анимация удара*
@@ -23,8 +24,29 @@
 враг не может наносить удар чаще какого-то времени. Когда игрок "ударен", показывается сообщение.
 Как вариант, после удара игрок отбрасывается на некоторое расстояние.
 
-}
+*Версия 7. Движение врага*
+Враг двигается горизонтально в сторону игрока. Если дошёл до конца экрана - исчезает.
 
+*Версия 8. Больше врагов*
+Враги появляются с некоторой периодичностью с разных сторон экрана в разных точках экрана.
+Исчезнувшие враги появляются снова.
+
+*Версия 9. Хитпоинты*
+У игрока и у врагов появляются хитпоинты.
+Хитпоинты игрока показываются на экране, хитпоинты врагов скрыты. один удар снимает один хитпоинт.
+Враг исчезает только когда хитпоинтов 0. Когда хитпоинты игрока 0 - игра заканчивается.
+
+*Версия 10. Лучники*
+Новый тип врагов: лучники. Они вместо удара запускают снаряд, который летит горизонтально.
+Если игрок коснулся снаряда - это считается ударом.
+Обычные враги теперь не просто идут по горизонтали, а идут в сторону игрока
+}
+{
+updates
+collision верх и низ немного сгладил
+
+
+}
 
 program arcFighter;
 
@@ -49,12 +71,11 @@ type
 		speed:Integer;
 	end;
 
-
 var
-	x,y,yEnemy, speedHero, timeOfHit, timeOfHitEnemy: Integer;
-	hitHero, hit: boolean; 
+	timeOfHit, timeOfHitEnemy: Integer;
+	hitHero: boolean; 
 	Hero : Thero;
-	EnemySpeed,xEnemy : real;
+	EnemySpeed,xEnemy, yEnemy: real;
 	
 procedure StartGraphics();
 var
@@ -79,13 +100,11 @@ begin
 	bar(0,185,800,600);	
 end;
 
-
 procedure drowBackground();
 begin
 	drawSky();
 	drawGarden();
 end;
-
 
 procedure drawSpriteHero(var hero:Thero);
 begin
@@ -109,15 +128,19 @@ function hitEnemy(): boolean;
 begin
 	if (((Abs(hero.position.x-xEnemy)<85) and (Abs(hero.position.y-yEnemy)<90))) then		
 	begin
-		dec(timeOfHitEnemy);
-		if timeOfHitEnemy=0 then
+		if (timeOfHitEnemy>0) then
 		begin
-			timeOfHitEnemy:=300;
-			hitEnemy:=true;
-		end
-		else
-		begin
-			hitEnemy:=false;
+			dec(timeOfHitEnemy);
+			writeln(timeOfHitEnemy);
+			if (timeOfHitEnemy=0) then
+			begin
+				hitEnemy:=true;
+				timeOfHitEnemy:=300;
+			end
+			else
+			begin
+				hitEnemy:=false;
+			end;
 		end;
 	end;
 end;
@@ -126,8 +149,8 @@ function NotColisionSprite(): boolean;
 begin
 	if (((hero.position.x+39) >= (xEnemy-40)) and 
 		((hero.position.x-39) <= (xEnemy+40)) and
-		((hero.position.y+89) >= (yEnemy-90)) and
-		((hero.position.y-89) <= (yEnemy+90))) then
+		((hero.position.y+44) >= (yEnemy-45)) and
+		((hero.position.y-44) <= (yEnemy+45))) then
 	begin
 		NotColisionSprite:=true;
 	end
@@ -137,26 +160,45 @@ begin
 	end;
 end;
 
-procedure drawSpriteEnemy(var xEnemy:real ; var yEnemy:Integer);
+procedure drawSpriteEnemy(var xEnemy, yEnemy:real);
 begin
 	setColor(yellow);
 	setFillstyle(1,yellow);
-	bar(round(xEnemy-40),yEnemy-90,round(xEnemy+40),yEnemy+90);
+	bar(round(xEnemy-40),round(yEnemy-90),round(xEnemy+40),round(yEnemy+90));
 	if ((Abs(hero.position.x-xEnemy)<85) and (Abs(hero.position.y-yEnemy)<90) and (hitHero)) then
 	begin
 	 	xEnemy:=xEnemy+1000;
 	end;
 	if not NotColisionSprite() then
 	begin
-		xEnemy:=xEnemy + EnemySpeed; 
+		if (xEnemy-hero.position.x)>0 then
+		begin
+			//writeln('left');
+			xEnemy:=xEnemy - EnemySpeed;	
+		end
+		else
+		begin
+			//writeln('right');
+			xEnemy:=xEnemy + EnemySpeed; 
+		end;
+
+		if (yEnemy - Hero.position.y)>0 then
+		begin
+			//writeln('up');
+			yEnemy:=yEnemy - EnemySpeed;
+		end
+		else
+		begin
+			//writeln('down');
+			yEnemy:=yEnemy + EnemySpeed;
+		end
+
 	end;
 	if xEnemy>900 then
 	begin
 		xEnemy:=-40;
 	end
 end;
-
-
 
 function Keyboard():boolean;
 var button: char;
@@ -203,38 +245,43 @@ begin
 					end;
 			'e': timeOfHit:=20;
 			'q': timeOfHit:=20;
-			'1': xEnemy:=xEnemy +1;
 		end;
 	end;
 end;
 
 //main procedure
-
 begin
 	StartGraphics();
 
 	UpdateGraph(UpdateOff);
 	ClearDevice();
 
-	hero.position.x:=95; hero.position.y:=400;
+	hero.position.x:=200; hero.position.y:=400;
 	EnemySpeed:=0.3;
-	hero.speed:=1;
+	hero.speed:=3;
 	xEnemy:=500; yEnemy:=500;
 
-	timeOfHitEnemy:=10;
+	timeOfHitEnemy:=300;
 
 	while true do
 	begin
 
 		drowBackground();
 
-		
 		drawSpriteEnemy(xEnemy,yEnemy); //
 		drawSpriteHero(Hero);
 
-		if not (hitEnemy()) then
+		if   not (hitEnemy()) then
 		begin
-			Hero.position.x := Hero.position.x+30;
+			if NotColisionSprite() then
+			begin
+				Hero.position.x := Hero.position.x+90;
+			end
+			else
+			begin
+				Hero.position.x := Hero.position.x-90;
+			end;
+
 			setColor(white);
 			setFillstyle(1, white);
 			OutTextXY(100, 100, 'hitdsfasgadfgsfdfghgfj');
@@ -252,7 +299,18 @@ begin
 		begin
 			break;			
 		end;
+
+		if Hero.position.x>800 then
+		begin
+			hero.position.x:=100;
+		end;
+
+		if Hero.position.x<0 then
+		begin
+			hero.position.x:=300;
+		end;
 	end;
+
 
 	readkey();
 	closegraph();
